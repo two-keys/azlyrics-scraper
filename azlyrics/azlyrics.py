b46e854517037b5e4605a6d7c368c150f323551b
keys <dandys10@live.com>
2021-03-21 06:48:15 -0500

    Match forward slashes three times

--- azlyrics/azlyrics.py
+++ azlyrics/azlyrics.py
@@ -61,7 +61,7 @@ def songs(artist):
                     pass
                 elif song_tag.text:
                     url_name = song_tag['href']
-                    url_name = re.search('(?<=/).*(?=\.html)', url_name)
+                    url_name = re.search('(?<=/){3}.*(?=\.html)', url_name)
                     songs.append({"name": tag.text, "url": url_name})
 
     artist['albums'][album_name] = songs
