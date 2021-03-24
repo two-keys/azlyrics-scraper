3fcf1e6bb748b3859071c7b1cdcf7e76eececb09
keys <dandys10@live.com>
2021-03-24 03:59:58 -0500

    Import sleep

--- azlyrics/azlyrics.py
+++ azlyrics/azlyrics.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import re
+from time import sleep
 
 agent = 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) \
         Gecko/20100101 Firefox/24.0'
