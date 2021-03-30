667a702a7f77fba3bd6620db254c350396aad4b2
keys <dandys10@live.com>
2021-03-29 20:17:43 -0500

    Switch order of find_latest conditional

--- azlyrics/azlyrics.py
+++ azlyrics/azlyrics.py
@@ -37,10 +37,10 @@ def find_latest(url):
     query_url = 'http://web.archive.org/cdx/search/cdx?url=' + url + '&collapse=digest&from=20120903185847&to=20180720043037&output=json'
     print("Query_url:", query_url)
     urls = try_connection(5, query_url)
-    if urls is None:
-        urls = '[]'
-    else:
+    if urls is not None and urls.text is not None:
         urls = urls.text
+    else:
+        urls = '[]'
     parse_url = json.loads(urls) # gets json
     url_list = []
     for i in range(1,len(parse_url)):
